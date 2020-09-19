8eeba56f38e9616c87dd7faf32d3432df6f8758d
Jorge Barrios <jorgebarmza@gmail.com>
2020-09-19 17:17:04 -0500

    Add heun approximation.

--- solvers.py
+++ solvers.py
@@ -94,28 +94,42 @@ if __name__ == "__main__":
     def y_prime(x, y):
         return y
 
-    p1_euler_dict = dict(
-        filename='psd1_euler.txt',
-        decimals=3,
-        target=2.71828182846,
-        method=euler,
-        n_init=50,
+    e = 2.71828182846
+
+    # Problem 1.
+    p1_args = dict(
+        target=e,
         y_prime=y_prime,
         x_init=0,
         y_init=1,
         x_final=1,
     )
 
-    # p1_heun_dict = dict(
-    #     filename='p1_euler.txt',
-    #     decimals=3,
-    #     target=2.71828182846,
-    #     method=euler,
-    #     n_init=50,
-    #     y_prime=y_prime,
-    #     x_init=0,
-    #     y_init=1,
-    #     x_final=1,
-    # )
+    p1_euler_args = p1_args
+    p1_euler_args.update(
+        dict(
+            filename='p1_euler.txt',
+            method=euler,
+            decimals=3,
+            n_init=50,
+        ))
 
-    approximate(**p1_euler_dict)
+    p1_heun_args = p1_args
+    p1_heun_args.update(
+        dict(
+            filename='p1_heun.txt',
+            method=heun,
+            decimals=5,
+            n_init=10,
+        ))
+
+    # p1_runge_args = p1_args
+    # p1_heun_args.update(
+    #     dict(
+    #         filename='p1_runge.txt',
+    #         method=runge,
+    #         decimals=9,
+    #         n_init=10,
+    #     ))
+
+    approximate(**p1_heun_args)
